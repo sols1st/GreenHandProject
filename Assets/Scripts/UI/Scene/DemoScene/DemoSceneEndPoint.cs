7cc6de6b8b34704e03676477791eb0a6f81b4648
Fei <932183701@qq.com>
2025-10-20 16:21:25 +0800

    add interactable functions

---
+++ Assets/Scripts/UI/Scene/DemoScene/DemoSceneEndPoint.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class DemoSceneEndPoint : Interactable
+{
+    public override void TriggerOnClick()
+    {
+        Debug.Log("Demo Scene EndPoint Trigger On Click");
+    }
+}
