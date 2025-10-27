de06ddae9a6e25dcb7df0cd0225cb14aa7148f37
sols1st <sols1st@outlook.com>
2025-10-27 20:23:09 +0800

    update

--- Assets/Scripts/Interactable/Item.cs
+++ Assets/Scripts/Interactable/Item.cs
@@ -8,12 +8,15 @@ public class Item : Interactable
     public override void TriggerOnClick()
     {
         string[] cardsToCheck = { cardName };
+        Debug.Log($"1 {cardName}");
         if (!CardManager.Instance.HasCards(cardsToCheck))
         {
+            Debug.Log($"2 {cardName}");
             CardManager.Instance.GetNewCard(cardName);
         }
         else
         {
+            Debug.Log($"3 {cardName}");
             Debug.Log($"卡片 {cardName} 已被获取！");
         }
     }
