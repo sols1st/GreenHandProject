18ee78b56b2a71e2e7ac4d0165a56a36b42169e2
sols1st <sols1st@outlook.com>
2025-10-28 19:57:24 +0800

    fix

--- Assets/Scripts/NPC/NPC.cs
+++ Assets/Scripts/NPC/NPC.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class NPC : Interactable
 {
+    private string npcName;
     public override void TriggerOnClick()
     {
+        Debug.Log($"人物 {npcName} 被点击！");
+        npcName = gameObject.name;
+        DialogueManager.Instance.StartNPCDialogue(npcName);
     }
 }
