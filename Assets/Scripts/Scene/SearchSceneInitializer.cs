db340fe68fdc8d5d649ac406c55df7499b011d3e
sols1st <sols1st@outlook.com>
2025-10-29 11:32:51 +0800

    bgm added

--- Assets/Scripts/Scene/SearchSceneInitializer.cs
+++ Assets/Scripts/Scene/SearchSceneInitializer.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class SearchSceneCursor : MonoBehaviour
 {
+    [SerializeField] string bgmName = null;
     private void OnEnable()
     {
+        AudioManager.Instance.PlayBackgroundMusic(bgmName);
         CursorManager.Instance.EnableCustomCursor();    
     }
 
     private void OnDisable()
     {
+        AudioManager.Instance.PlayBackgroundMusic(null);
         CursorManager.Instance.DisableCustomCursor();    
     }
 
